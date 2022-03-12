101d954b65c5c88df3f568fcb050b14231abce6e
ZitaRR <Haeggqvist1998@hotmail.com>
2022-03-12 17:59:16 +0100

    Forgot to include in previous commit

---
+++ js/main.js
@@ -0,0 +1,37 @@
+document.addEventListener("DOMContentLoaded", async () => {
+    console.log("DOM loaded");
+    await info();
+});
+
+async function info(){
+    const info = document.getElementById("user-info");
+    if(!info){
+        return;
+    }
+
+    console.log(navigator.userAgent);
+    const response = await fetch("https://ipapi.co/json/")
+    if(!response.ok){
+        console.log("Failed to fetch IP");
+        return;
+    }
+
+    const json = await response.json();
+    console.log(json);
+
+    info.innerHTML = `
+    Your location: ${json.region}, ${json.country_name}<br>
+    IP: ${json.ip}<br>
+    Service: ${json.org}<br>
+    `;
+}
+
+function toggleHamburger(){
+    let nav = document.getElementById("nav");
+    if(nav.className === "navbar frame-buttom"){
+        nav.className += " responsive";
+    }
+    else {
+        nav.className = "navbar frame-buttom";
+    }
+}
