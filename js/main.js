b1c95264cde4cbc254c344f4c8d2352e6a0fcf6d
ZitaRR <Haeggqvist1998@hotmail.com>
2022-03-18 22:31:40 +0100

    Shit ton of stuff

--- js/main.js
+++ js/main.js
@@ -1,32 +1,70 @@
-document.addEventListener("DOMContentLoaded", async () => {
+document.addEventListener("DOMContentLoaded", () => {
     console.log("DOM loaded");
-    await info();
 });
 
-async function info(){
-    const info = document.getElementById("user-info");
-    if(!info){
+document.addEventListener("scroll", () => {
+    const home = document.getElementById("home");
+    if(isElementWithinView(home)){
+        setActiveNav(document.getElementById("nav-home"));
         return;
     }
 
-    console.log(navigator.userAgent);
-    const response = await fetch("https://ipapi.co/json/")
-    if(!response.ok){
-        console.log("Failed to fetch IP");
+    const about = document.getElementById("about");
+    if(isElementWithinView(about)){
+        setActiveNav(document.getElementById("nav-about"));
         return;
     }
-
-    const json = await response.json();
-    console.log(json);
-
-    info.innerHTML = `
-    Your location: ${json.region}, ${json.country_name}<br>
-    IP: ${json.ip}<br>
-    Service: ${json.org}<br>
-    `;
-}
+});
 
 function toggleHamburger(){
-    let nav = document.getElementById("nav");
+    const nav = document.getElementById("nav");
     nav.classList.toggle("responsive");
+}
+
+function goToSection(id){
+    const section = document.getElementById(id);
+    if(!section){
+        return;
+    }
+
+    const nav = document.getElementById("nav");
+    if(nav && nav.classList.contains("responsive")){
+        nav.classList.toggle("responsive");
+    }
+
+    section.scrollIntoView({behavior: "smooth"});
+}
+
+// should have a better name but im a retard
+// and i cant think of a better name atm
+function isElementWithinView(element){
+    if(!element){
+        return;
+    }
+
+    const rect = element.getBoundingClientRect();
+    const absolute = Math.abs(rect.y);
+
+    if(absolute > rect.height / 2){
+        return false;
+    }
+
+    return true;
+}
+
+function setActiveNav(nav){
+    if(!nav){
+        return;
+    }
+    else if(!nav.id.includes("nav-")){
+        return;
+    }
+
+    const current = document.getElementsByClassName("active")[0];
+    if(nav.isSameNode(current)){
+        return;
+    }
+
+    current.classList.toggle("active");
+    nav.classList.toggle("active");
 }
